529e99c1450de1b2692587c7abb0da25cb5854f6
Phat <6652@ait.nsw.edu.au>
2022-03-07 17:40:28 +1100

    Update section scroll animation
    Peter

--- src/components/BannerWelcome.js
+++ src/components/BannerWelcome.js
@@ -1,36 +1,60 @@
 import React from "react";
 import "./App.css"
 import { Link } from "react-scroll"
+import { Navbar, Container, Nav} from "react-bootstrap";
 
 
 const BannerWelcome = () => {
  return (
   <div className="title">
    <h1 >welcome<br/><br/>recruiter</h1>
-   <div className="sideMenu">
-    <Link
-     to="profileSection" 
-     spy={true} 
-     smooth={true} 
-     offset={-5} 
-     duration={500}>
-     My Profile</Link>
-    <Link
-     to="expSection" 
-     spy={true} 
-     smooth={true} 
-     offset={-5} 
-     duration={500}>
-     Experiences</Link>
-     <Link
-     to="skillSection" 
-     spy={true} 
-     smooth={true} 
-     offset={-5} 
-     duration={500}>
-     Skills</Link>
-   </div>
-   
+   <div className="sideMenu" >
+   <Navbar expand="lg" >
+    <Container style={{display: "flex !important",  flexDirection: "row", justifyContent: "end", backgroundColor: "transparent"}}>
+    <Navbar.Toggle 
+     aria-controls="basic-navbar-nav" 
+    >
+     <i class="arrow square down icon"></i>
+    </Navbar.Toggle>
+    <Navbar.Collapse id="basic-navbar-nav" >
+      <Nav className="me-auto" >
+        <Nav.Link style={{border: "0.5px solid black"}}>
+         <Link
+          to="profileSection" 
+          spy={true} 
+          smooth={true} 
+          offset={-5} 
+          duration={300}>
+          My Profile
+         </Link>
+        </Nav.Link>
+        
+        <Nav.Link style={{border: "0.5px solid black"}}>
+         <Link
+          to="expSection" 
+          spy={true} 
+          smooth={true} 
+          offset={-5} 
+          duration={300}>
+          Experiences
+         </Link>
+        </Nav.Link>
+
+        <Nav.Link style={{border: "0.5px solid black"}}>
+         <Link
+          to="skillSection" 
+          spy={true} 
+          smooth={true} 
+          offset={-5} 
+          duration={300}>
+          Skills
+         </Link>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+   </div>  
   </div>
  )
 }
