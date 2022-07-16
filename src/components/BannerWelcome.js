36b5aee0bedc8c860f3375ccac85b25caa4db9c8
Phat <100731847+Pipi144@users.noreply.github.com>
2022-07-16 20:11:35 +0700

    Add some Responsive Adjustment
    Peter

--- src/components/BannerWelcome.js
+++ src/components/BannerWelcome.js
@@ -1,62 +1,75 @@
-import React from "react";
-import "./App.css"
-import { Link } from "react-scroll"
-import { Navbar, Container, Nav} from "react-bootstrap";
-
+import React from 'react'
+import './App.css'
+import { Link } from 'react-scroll'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const BannerWelcome = () => {
- return (
-  <div className="title">
-   <h1 >welcome<br/><br/>recruiter</h1>
-   <div className="sideMenu" >
-   <Navbar expand="lg" expand= {false}>
-    <Container style={{display: "flex !important",  flexDirection: "row", justifyContent: "end", backgroundColor: "transparent", alignItems: "end"}}>
-    <Navbar.Toggle 
-     aria-controls="basic-navbar-nav" 
-    >
-     <i className="arrow square down icon big inverted"></i>
-    </Navbar.Toggle>
-    <Navbar.Collapse id="basic-navbar-nav" >
-      <Nav className="me-auto" >
-        <Nav.Link style={{border: "0.5px solid black"}}>
-         <Link
-          to="profileSection" 
-          spy={true} 
-          smooth={true} 
-          offset={-5} 
-          duration={300}>
-          My Profile
-         </Link>
-        </Nav.Link>
-        
-        <Nav.Link style={{border: "0.5px solid black"}}>
-         <Link
-          to="expSection" 
-          spy={true} 
-          smooth={true} 
-          offset={-5} 
-          duration={300}>
-          Experiences
-         </Link>
-        </Nav.Link>
+  return (
+    <div className='title'>
+      <h1>
+        welcome
+        <br />
+        <br />
+        recruiter
+      </h1>
+      <div className='sideMenu'>
+        <Navbar expand='lg' expand={false}>
+          <Container
+            style={{
+              display: 'flex !important',
+              flexDirection: 'row',
+              justifyContent: 'end',
+              backgroundColor: 'transparent',
+              alignItems: 'end',
+            }}
+          >
+            <Navbar.Toggle aria-controls='basic-navbar-nav'>
+              <i className='arrow square down icon big inverted'></i>
+            </Navbar.Toggle>
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto'>
+                <Nav.Link style={{ border: '0.5px solid black' }}>
+                  <Link
+                    to='profileSection'
+                    spy={true}
+                    smooth={true}
+                    offset={-5}
+                    duration={300}
+                  >
+                    My Profile
+                  </Link>
+                </Nav.Link>
 
-        <Nav.Link style={{border: "0.5px solid black"}}>
-         <Link
-          to="skillSection" 
-          spy={true} 
-          smooth={true} 
-          offset={-5} 
-          duration={300}>
-          Skills
-         </Link>
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-   </div>  
-  </div>
- )
+                <Nav.Link style={{ border: '0.5px solid black' }}>
+                  <Link
+                    to='expSection'
+                    spy={true}
+                    smooth={true}
+                    offset={-5}
+                    duration={300}
+                  >
+                    Experiences
+                  </Link>
+                </Nav.Link>
+
+                <Nav.Link style={{ border: '0.5px solid black' }}>
+                  <Link
+                    to='skillSection'
+                    spy={true}
+                    smooth={true}
+                    offset={-5}
+                    duration={300}
+                  >
+                    Skills
+                  </Link>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
+  )
 }
 
-export default BannerWelcome;
+export default BannerWelcome
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,35 @@
-import React from "react";
-import "./App.css"
-import "./Header.scss"
+import React from 'react'
+import './App.css'
+import './Header.scss'
 
 const Header = () => {
- return (
-<div className="aos" data-aos="fade-out">
-  <div className="header">
-    <div className="ui labeled icon menu headerStyle">
-      <a className="item" href="https://www.linkedin.com/feed/" target="_blank">
-      <i className="inverted linkedin icon"></i>
-      LinkedIn
-      </a>
-      <a className="item" href="https://www.facebook.com/truongmp/" target="_blank">
-        <i class="inverted facebook f icon"></i>
-        Facebook
-      </a>
-      <div className="right menu menuSignature wrapper">
-       <h1>I'm PiPi</h1>
+  return (
+    <div className='aos' data-aos='fade-out'>
+      <div className='header'>
+        <div className='ui labeled icon menu headerStyle'>
+          <a
+            className='item'
+            href='https://www.linkedin.com/feed/'
+            target='_blank'
+          >
+            <i className='inverted linkedin icon'></i>
+            LinkedIn
+          </a>
+          <a
+            className='item'
+            href='https://www.facebook.com/truongmp/'
+            target='_blank'
+          >
+            <i class='inverted facebook f icon'></i>
+            Facebook
+          </a>
+          <div className='right menu menuSignature wrapper'>
+            <h1>I'm Peter</h1>
+          </div>
+        </div>
       </div>
-   </div>
-  </div>
-  </div>
- );
+    </div>
+  )
 }
 
-export default Header;
+export default Header
